1e7134df1bcab2a19ee066d0a56020288dd1c852
Kieran T <kieran123416@gmail.com>
2025-07-05 17:01:24 +0100

    Blank Project
    Initial Commit to the project of it being blank

---
+++ Source/SpaceStealth/SpaceStealth.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SpaceStealth.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, SpaceStealth, "SpaceStealth" );
