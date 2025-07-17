2b57a5abc824ae05871d2f49007129ed87c2190c
Kieran T <kieran123416@gmail.com>
2025-07-17 21:18:35 +0100

    Attribute sets setup

---
+++ Source/SpaceStealth/Public/Attributes/BaseAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSetDefinitions.h"
+#include "BaseAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACESTEALTH_API UBaseAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
